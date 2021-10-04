dbdc14288a2c4cd8256edff7992266ac38ed46c2
Sam Deane <sam@elegantchaos.com>
2021-10-04 14:43:37 +0100

    Fixed module name.

--- Package.swift
+++ Package.swift
@@ -8,24 +8,24 @@
 import PackageDescription
 
 let package = Package(
-    name: "CSVEncoder",
+    name: "CSVCoding",
     platforms: [
         .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)
     ],
     products: [
         .library(
-            name: "CSVEncoder",
-            targets: ["CSVEncoder"]),
+            name: "CSVCoding",
+            targets: ["CSVCoding"]),
     ],
     dependencies: [
         .package(url: "https://github.com/elegantchaos/XCTestExtensions.git", from: "1.3.3")
     ],
     targets: [
         .target(
-            name: "CSVEncoder",
+            name: "CSVCoding",
             dependencies: []),
         .testTarget(
-            name: "CSVEncoderTests",
-            dependencies: ["CSVEncoder", "XCTestExtensions"]),
+            name: "CSVCodingTests",
+            dependencies: ["CSVCoding", "XCTestExtensions"]),
     ]
 )
